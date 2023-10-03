fe110e43a4d00a13e198475c12efdac955e62d53
Godfrey Karugu <karugugk@gmail.com>
2023-10-03 14:47:05 +0300

    completed lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 
-// Write your code here
+function multiply(){
+ let  multiply = 62;
+ let num1;
+ let num2;
+
+  multiply = num1 * num2
+}
+
+function random(max, min){
+  Math.floor(Math.random() * (max-min)) + min;
+}
+
+
+function mod(){
+  let num3;
+  let num4;
+  if(num4 !==0){
+    return num3 % num4;
+  }
+}
+
+
+
+function max(){
+  const arr = []
+  for (i = 1; 1 < 20; i++)
+  {
+    arr[i] = i;
+  }
+
+  return Math.max(arr)
+}
