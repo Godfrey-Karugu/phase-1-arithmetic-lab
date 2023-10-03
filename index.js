c162a2560ec525389721c0298dcc5d81216abfff
Godfrey Karugu <karugugk@gmail.com>
2023-10-03 17:01:12 +0300

    improved version of the assignment

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 
-function multiply(){
- let  multiply = 62;
- let num1;
- let num2;
+// function multiply(){
+//  let  multiply = 62;
+//  let num1;
+//  let num2;
 
-  multiply = num1 * num2
-}
+//   multiply = num1 * num2
+// }
 
-function random(max, min){
-  Math.floor(Math.random() * (max-min)) + min;
-}
-
-
-function mod(){
-  let num3;
-  let num4;
-  if(num4 !==0){
-    return num3 % num4;
-  }
-}
+// function random(max, min){
+//   Math.floor(Math.random() * (max-min)) + min;
+// }
 
 
+// function mod(){
+//   let num3;
+//   let num4;
+//   if(num4 !==0){
+//     return num3 % num4;
+//   }
+// }
 
-function max(){
-  const arr = []
-  for (i = 1; 1 < 20; i++)
-  {
-    arr[i] = i;
-  }
+let num1 = 31;
+let num2 = 2 ;
 
-  return Math.max(arr)
-}
+let multiply ;
+    multiply = num1 * num2;
+
+  //  Create a variable called `random` that will generate a random integer greater
+  //   than 0.
+
+let random;
+
+random = Math.floor(Math.random()* 10) + 1
+
+// Create a variable called `mod` set to an equation that will calculate the
+//    remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+// Create a variable called `max` that finds the highest number in a set; the
+//    value returned should be `20`.
+
+let max;
+ max = Math.max(3,18,6,20,9,2,14)
+ 
